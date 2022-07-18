PixelExperience/packages_apps_Settings

Restore power menu settings from eleven-plus
commit 8a9b0f237414b87d486951410c25a667ff5e75ef (HEAD -> plus)
Author: Gegham Zakaryan <zakaryan.2004@outlook.com>
Date:   Sun Apr 4 15:52:24 2021 +0400

    PowerMenuActions: Add emergency option

    This allows toggling Emergency button in GlobalAction(Power menu).

    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I190048e213a08a969e10aef4081650875dab2587

commit 1fa71881128027e81b30656d2e3dec1211d4cc26
Author: Gegham Zakaryan <zakaryan.2004@outlook.com>
Date:   Sun Apr 4 15:45:42 2021 +0400

    PowerMenuActions: Add more checks for lockdown option states

    Use LockPatternUtils to check if lock screen is secure.
    Use UserManager and UserInfo to check if the current user is primary.

    In result, bugreport and lockdown are visible and toggled only during correct scenarios:
            - Bug reporting option is disabled if development settings aren't on
            - Bug reporting option is disabled if the current user isn't the primary user
            - Bug reporting checkbox reflects the state of Settings.Global.BUGREPORT_IN_POWER_MENU
            - Lockdown option is disabled if keyguard isn't secure
            - Lockdown checkbox reflects the state of Settings.Secure.LOCKDOWN_IN_POWER_MENU

    String summaries are added for each scenario.

    jhenrique09 edits:
     - Remove bugreport

    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I8a4e4a6078e8277ce2cfee16d5b2abb4de91c746

commit d27ba7f0a1672396cef080cee5acb50197145d0d
Author: Gegham Zakaryan <zakaryan.2004@outlook.com>
Date:   Sun Apr 4 15:39:40 2021 +0400

    PowerMenuActions: Use methods from fwb

    The methods now live in a separate GlobalActionsManagerService inside fwb.

    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: I58dde8e75460743428ef51309a8a31b997292cf1

Change-Id: Ie255575f1cd5e140fc7fc9f8f696a6629d18fc4d

src/com/android/settings/custom/buttons/PowerMenuActions.java
@@ -1,0 +1,187 @@
+/*
+ * Copyright (C) 2014-2015 The CyanogenMod Project
+ *               2017-2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.custom.buttons;
+
+import static com.android.internal.util.custom.globalactions.PowerMenuConstants.*;
+
+import android.content.Context;
+import android.content.pm.UserInfo;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.provider.Settings;
+
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.internal.util.custom.globalactions.CustomGlobalActions;
+import com.android.internal.util.custom.globalactions.PowerMenuConstants;
+import com.android.internal.widget.LockPatternUtils;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class PowerMenuActions extends SettingsPreferenceFragment {
+    final static String TAG = "PowerMenuActions";
+
+    private CheckBoxPreference mScreenshotPref;
+    private CheckBoxPreference mAirplanePref;
+    private CheckBoxPreference mUsersPref;
+    private CheckBoxPreference mLockDownPref;
+    private CheckBoxPreference mEmergencyPref;
+
+    private CustomGlobalActions mCustomGlobalActions;
+
+    Context mContext;
+    private LockPatternUtils mLockPatternUtils;
+    private UserManager mUserManager;
+    private List<String> mLocalUserConfig = new ArrayList<String>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.power_menu_shortcuts);
+        mContext = getActivity().getApplicationContext();
+        mCustomGlobalActions = CustomGlobalActions.getInstance(mContext);
+        mLockPatternUtils = new LockPatternUtils(mContext);
+        mUserManager = UserManager.get(mContext);
+
+        for (String action : PowerMenuConstants.getAllActions()) {
+            if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
+                mScreenshotPref = findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
+            } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
+                mAirplanePref = findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
+            } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
+                mUsersPref = findPreference(GLOBAL_ACTION_KEY_USERS);
+            } else if (action.equals(GLOBAL_ACTION_KEY_LOCKDOWN)) {
+                mLockDownPref = findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
+            } else if (action.equals(GLOBAL_ACTION_KEY_EMERGENCY)) {
+                mEmergencyPref = findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
+            }
+        }
+
+        mLocalUserConfig = mCustomGlobalActions.getLocalUserConfig();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (mScreenshotPref != null) {
+            mScreenshotPref.setChecked(mCustomGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_SCREENSHOT));
+        }
+
+        if (mAirplanePref != null) {
+            mAirplanePref.setChecked(mCustomGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_AIRPLANE));
+        }
+
+        if (mLockDownPref != null) {
+            mLockDownPref.setChecked(mCustomGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_LOCKDOWN));
+        }
+
+        if (mUsersPref != null) {
+            if (!UserHandle.MU_ENABLED || !UserManager.supportsMultipleUsers()) {
+                getPreferenceScreen().removePreference(findPreference(GLOBAL_ACTION_KEY_USERS));
+                mUsersPref = null;
+            } else {
+                List<UserInfo> users = mUserManager.getUsers();
+                boolean enabled = (users.size() > 1);
+                mUsersPref.setChecked(mCustomGlobalActions.userConfigContains(
+                        GLOBAL_ACTION_KEY_USERS) && enabled);
+                mUsersPref.setEnabled(enabled);
+            }
+        }
+
+        if (mEmergencyPref != null) {
+            mEmergencyPref.setChecked(mCustomGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_EMERGENCY));
+        }
+
+        updatePreferences();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updatePreferences();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        boolean value;
+
+        if (preference == mScreenshotPref) {
+            value = mScreenshotPref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENSHOT);
+
+        } else if (preference == mAirplanePref) {
+            value = mAirplanePref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_AIRPLANE);
+
+        } else if (preference == mUsersPref) {
+            value = mUsersPref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_USERS);
+
+        } else if (preference == mLockDownPref) {
+            value = mLockDownPref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_LOCKDOWN);
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.LOCKDOWN_IN_POWER_MENU, value ? 1 : 0, UserHandle.USER_CURRENT);
+
+        } else if (preference == mEmergencyPref) {
+            value = mEmergencyPref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_EMERGENCY);
+
+        } else {
+            return super.onPreferenceTreeClick(preference);
+        }
+        return true;
+    }
+
+    private void updatePreferences() {
+        boolean isKeyguardSecure = mLockPatternUtils.isSecure(UserHandle.myUserId());
+        boolean lockdown = Settings.Secure.getIntForUser(
+                getContentResolver(), Settings.Secure.LOCKDOWN_IN_POWER_MENU, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (mLockDownPref != null) {
+            mLockDownPref.setEnabled(isKeyguardSecure);
+            if (isKeyguardSecure) {
+                mLockDownPref.setChecked(lockdown);
+                mLockDownPref.setSummary(null);
+            } else {
+                mLockDownPref.setChecked(false);
+                mLockDownPref.setSummary(R.string.power_menu_shortcut_unavailable);
+            }
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CUSTOM_SETTINGS;
+    }
+}

paperboy72543/android_packages_apps_Settings

src/com/android/settings/fusion/buttons/PowerMenuActions.java
@@ -1,0 +1,187 @@
+/*
+ * Copyright (C) 2014-2015 The CyanogenMod Project
+ *               2017-2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fusion.buttons;
+
+import static com.android.internal.util.custom.globalactions.PowerMenuConstants.*;
+
+import android.content.Context;
+import android.content.pm.UserInfo;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.provider.Settings;
+
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.internal.util.custom.globalactions.CustomGlobalActions;
+import com.android.internal.util.custom.globalactions.PowerMenuConstants;
+import com.android.internal.widget.LockPatternUtils;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+public class PowerMenuActions extends SettingsPreferenceFragment {
+    final static String TAG = "PowerMenuActions";
+
+    private CheckBoxPreference mScreenshotPref;
+    private CheckBoxPreference mAirplanePref;
+    private CheckBoxPreference mUsersPref;
+    private CheckBoxPreference mLockDownPref;
+    private CheckBoxPreference mEmergencyPref;
+
+    private CustomGlobalActions mCustomGlobalActions;
+
+    Context mContext;
+    private LockPatternUtils mLockPatternUtils;
+    private UserManager mUserManager;
+    private List<String> mLocalUserConfig = new ArrayList<String>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.power_menu_shortcuts);
+        mContext = getActivity().getApplicationContext();
+        mCustomGlobalActions = CustomGlobalActions.getInstance(mContext);
+        mLockPatternUtils = new LockPatternUtils(mContext);
+        mUserManager = UserManager.get(mContext);
+
+        for (String action : PowerMenuConstants.getAllActions()) {
+            if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
+                mScreenshotPref = findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
+            } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
+                mAirplanePref = findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
+            } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
+                mUsersPref = findPreference(GLOBAL_ACTION_KEY_USERS);
+            } else if (action.equals(GLOBAL_ACTION_KEY_LOCKDOWN)) {
+                mLockDownPref = findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
+            } else if (action.equals(GLOBAL_ACTION_KEY_EMERGENCY)) {
+                mEmergencyPref = findPreference(GLOBAL_ACTION_KEY_EMERGENCY);
+            }
+        }
+
+        mLocalUserConfig = mCustomGlobalActions.getLocalUserConfig();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (mScreenshotPref != null) {
+            mScreenshotPref.setChecked(mCustomGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_SCREENSHOT));
+        }
+
+        if (mAirplanePref != null) {
+            mAirplanePref.setChecked(mCustomGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_AIRPLANE));
+        }
+
+        if (mLockDownPref != null) {
+            mLockDownPref.setChecked(mCustomGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_LOCKDOWN));
+        }
+
+        if (mUsersPref != null) {
+            if (!UserHandle.MU_ENABLED || !UserManager.supportsMultipleUsers()) {
+                getPreferenceScreen().removePreference(findPreference(GLOBAL_ACTION_KEY_USERS));
+                mUsersPref = null;
+            } else {
+                List<UserInfo> users = mUserManager.getUsers();
+                boolean enabled = (users.size() > 1);
+                mUsersPref.setChecked(mCustomGlobalActions.userConfigContains(
+                        GLOBAL_ACTION_KEY_USERS) && enabled);
+                mUsersPref.setEnabled(enabled);
+            }
+        }
+
+        if (mEmergencyPref != null) {
+            mEmergencyPref.setChecked(mCustomGlobalActions.userConfigContains(
+                    GLOBAL_ACTION_KEY_EMERGENCY));
+        }
+
+        updatePreferences();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updatePreferences();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        boolean value;
+
+        if (preference == mScreenshotPref) {
+            value = mScreenshotPref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENSHOT);
+
+        } else if (preference == mAirplanePref) {
+            value = mAirplanePref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_AIRPLANE);
+
+        } else if (preference == mUsersPref) {
+            value = mUsersPref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_USERS);
+
+        } else if (preference == mLockDownPref) {
+            value = mLockDownPref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_LOCKDOWN);
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.LOCKDOWN_IN_POWER_MENU, value ? 1 : 0, UserHandle.USER_CURRENT);
+
+        } else if (preference == mEmergencyPref) {
+            value = mEmergencyPref.isChecked();
+            mCustomGlobalActions.updateUserConfig(value, GLOBAL_ACTION_KEY_EMERGENCY);
+
+        } else {
+            return super.onPreferenceTreeClick(preference);
+        }
+        return true;
+    }
+
+    private void updatePreferences() {
+        boolean isKeyguardSecure = mLockPatternUtils.isSecure(UserHandle.myUserId());
+        boolean lockdown = Settings.Secure.getIntForUser(
+                getContentResolver(), Settings.Secure.LOCKDOWN_IN_POWER_MENU, 0,
+                UserHandle.USER_CURRENT) == 1;
+        if (mLockDownPref != null) {
+            mLockDownPref.setEnabled(isKeyguardSecure);
+            if (isKeyguardSecure) {
+                mLockDownPref.setChecked(lockdown);
+                mLockDownPref.setSummary(null);
+            } else {
+                mLockDownPref.setChecked(false);
+                mLockDownPref.setSummary(R.string.power_menu_shortcut_unavailable);
+            }
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.FUSION;
+    }
+}